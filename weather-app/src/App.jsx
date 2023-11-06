7d023548e51e519d04fd1e09ccded223ef48c87c
trithecoder <triyelnewry@gmail.com>
2023-11-06 17:03:38 -0500

    Publish

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import WeatherApp from "./Components/WeatherApp/WeatherApp"
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <WeatherApp />
+      </div>
+    </>
+  );
+}
+
+export default App;
